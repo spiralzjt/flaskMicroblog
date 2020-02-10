b4cf1c2a7d88f6caae9731ca09e6d9b7b11b56f4
zjt <stardustzjt@gmail.com>
2020-02-10 23:03:54 +0800

    implements user login and registration with werkzeug and flask-login

--- app/models.py
+++ app/models.py
@@ -1,15 +1,24 @@
 from datetime import datetime
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+# inherit from usermixin, which provides default implementations for 4 modules: is_authenticated, is_active, is_anonymous, get_id().
+# not required, but recommend from flask-login
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-    #For a one-to-many relationship, a db.relationship field is normally defined on the "one" side, and is used as a convenient way to get access to the "many"
+    # For a one-to-many relationship, a db.relationship field is normally defined on the "one" side, and is used as a convenient way to get access to the "many"
 
-#The __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# The __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -21,3 +30,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
-from app import app
-from flask import render_template, flash, redirect
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app.models import User
 
 #修饰器decorator，when a web browser requests either of these two URLs,
 #Flask is going to invoke this function and pass the return value of it back to the browser as a response
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Zjt'}
     posts = [
         {
             'author' : {'username':'Jon'},
@@ -18,15 +21,43 @@ def index():
             'body' : 'Driving a car in US is great!'
         }
     ]
-    return render_template('index.html', title='home', user=user, posts = posts)
+    return render_template('index.html', title='home', posts = posts)
     #This function takes a template filename and a variable list of template arguments and returns the same template, but with all the placeholders in it replaced with actual values.
 
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    # if the user already logged in and access to /index dir, it gonna be redirected to /index
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        # processed in the base.html with get_flashed_messages() function
-        flash('Login request for user {}, remember_me = {}'.format(form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
