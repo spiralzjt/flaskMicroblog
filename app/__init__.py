d120517e1a61a6558aac7b088323ce0746ed0a0c
zjt <stardustzjt@gmail.com>
2020-02-10 11:27:06 +0800

    add database with sqlalchemy and migrate

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@
 #and this file tell the app package to import other objects
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  #import at last to avoid circular imports
+from app import routes, models  #import at last to avoid circular imports
---
+++ app/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    #For a one-to-many relationship, a db.relationship field is normally defined on the "one" side, and is used as a convenient way to get access to the "many"
+
+#The __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
