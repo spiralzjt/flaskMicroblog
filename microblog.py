5fdd91d8c800791be5a47e57ddb2c76b17dd74ff
zjt <stardustzjt@gmail.com>
2020-02-10 11:59:57 +0800

    use sqlalchemy in shell and configure flask shell

--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
 from app import app, db
+from app.models import User, Post
+
+#registers the function as a shell context function, When the flask shell command runs, it will invoke this function
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
