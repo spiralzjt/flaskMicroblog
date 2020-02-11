1aa7c41dfc85852e1ed656bb60072d6220e76767
zjt <stardustzjt@gmail.com>
2020-02-11 17:53:56 +0800

    implements avatar and changing profile, also improve the design call using jinjia2 sub-template

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
+from datetime import datetime
 
 #修饰器decorator，when a web browser requests either of these two URLs,
 #Flask is going to invoke this function and pass the return value of it back to the browser as a response
@@ -61,3 +62,37 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts=[
+        {'author':user, 'body': 'Test Post #1'},
+        {'author':user, 'body': 'Test Post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+# consider that when you reference current_user, Flask-Login will invoke the user loader callback function,
+#  which will run a database query that will put the target user in the database session, so no need for the db.session.add()
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
