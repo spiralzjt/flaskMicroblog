ed0ca6844f74fb78acc4efc3920ca07c067bb5af
zjt <stardustzjt@gmail.com>
2020-02-13 17:39:42 +0800

    implements twist follow many-to-many relationship by modifying the database relation in model.py, and write a unit test with unittest depandency, then change the front-end and routes to check the function of follow relationship

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 # inherit from usermixin, which provides default implementations for 4 modules: is_authenticated, is_active, is_anonymous, get_id().
 # not required, but recommend from flask-login
 class User(UserMixin, db.Model):
@@ -11,10 +16,20 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic')   # For a one-to-many relationship, a db.relationship field is normally defined on the "one" side, and is used as a convenient way to get access to the "many"
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    # For a one-to-many relationship, a db.relationship field is normally defined on the "one" side, and is used as a convenient way to get access to the "many"
+    # the left side user is following the right side user
+    # secondary configures the association table that is used for this relationship, which I defined right above this class
+    # backref defines how this relationship will be accessed from the right side entity
+    # primaryjoin indicates the condition that links the left side entity (the follower user) with the association table
+    # secondaryjoin indicates the condition that links the right side entity (the followed user) with the association table
+    # The additional lazy argument indicates the execution mode for this query. A mode of dynamic sets up the query to not run until specifically requested
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -24,6 +39,35 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    # get the posts of all the users self.user is following
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
+
+    # get self.user's own posts and merger with the posts from all the users he or she follows
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
+
 
 # The __repr__ method tells Python how to print objects of this class, which is going to be useful for debugging
     def __repr__(self):
@@ -33,7 +77,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))        # define foreign key on the "many" side
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
