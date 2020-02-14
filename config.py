5a1b34abeb863147f9262beb7de95eb57e9a0a81
spiralzjt <stardustzjt@gmail.com>
2020-02-14 17:52:05 +0800

    get to know paginate() in SQLAlchemy to paginate the posts

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    POST_PER_PAGE = 3
+
     # add the email server details to the configuration file
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
